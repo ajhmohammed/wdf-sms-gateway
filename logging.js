71c70a6d5e18fe030f9f59d5d4ee0936210c8f78
ajhmohammed <50343026+ajhmohammed@users.noreply.github.com>
2024-10-22 20:29:04 +0530

    updated logging

--- logging.js
+++ logging.js
@@ -1,25 +1,50 @@
 var fs = require('node:fs')
+const path = require('node:path')
 
 const logging = (logLevel, message) => {
 
-    // log file
-    logFileName = 'logs.txt'
+    // Create a seperate folder for logging
+    const folderName = 'logs'
 
-    // Date time
-    logTime = new Date().toLocaleString();
+    if (!fs.existsSync(folderName)) {
+        fs.mkdir(folderName, {recursive:true}, (err) => {
+            if(err) {
+                console.error(`Error appending to file: ${err}`);
+            } else {
+                console.log('Content appended successfully.');
+                appendToFile();
+            }
+        });
+    } else {
+        appendToFile();
+    }
 
-    // log level [Error, Info, warn]
-    logLevel = "[" + logLevel + "]";
+    function appendToFile() {
 
-    // Message
-    message = message; 
+        //Log filename based on the date
+        let today = new Date();
+        let year = today.getFullYear()
+        let month = String(today.getMonth() + 1).padStart(2, '0')
+        let date = String(today.getDate()).padStart(2, '0')
 
-    logMessage = logTime + "\t" + logLevel + "\t" + message + "\n";
+        logFileName = `logs_${year+month+date}.txt`
 
-    console.log(logMessage);
+        // Date time
+        logTime = new Date().toLocaleString();
 
-    fs.appendFile(logFileName, logMessage, function (err) {})
+        // log level [Error, Info, warn]
+        logLevel = "[" + logLevel + "]";
 
+        // Message
+        message = message; 
+
+        logMessage = logTime + "\t" + logLevel + "\t" + message + "\n";
+
+        console.log(logMessage);
+
+        fs.appendFile(`${folderName}/${logFileName}`, logMessage, function (err) {})
+
+    }
 }
 
 module.exports = logging
