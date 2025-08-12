0051a6a6fb7dd382a3c6c38e126a62654dd15574
ajhmohammed <50343026+ajhmohammed@users.noreply.github.com>
2025-08-12 14:12:16 +0530

    Invalid code bug fixes and logging to logger

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.json());
 const cron = require('node-cron')
+const logging = require('../logging')
 
 const { processCommunityReferredClients } = require('./cron/communityReferredClients.js');
 const { processCommunityUpcomingAppointments } = require('./cron/communityUpcomingAppointment.js');
@@ -12,38 +13,38 @@ const { processClinicMissedAppointment } = require('./cron/clinicMissedAppointme
 
 // Schedule to run every day at 6 PM
 cron.schedule('0 18 * * *', async () => {
-  console.log(`[${new Date().toLocaleString()}] Cron triggered: Processing Community Referred Clients`);
+  logging(`[Info]`, `[${new Date().toLocaleString()}] Cron triggered: Processing Community Referred Clients`);
   await processCommunityReferredClients();
 
-  console.log(`[${new Date().toLocaleString()}] Cron triggered: Processing Community Missed Appointments`);
+  logging(`[Info]`, `[${new Date().toLocaleString()}] Cron triggered: Processing Community Missed Appointments`);
   await processCommunityMissedAppointments();
 
-  console.log(`[${new Date().toLocaleString()}] Cron triggered: Processing Clinic Missed Appointments`);
+  logging(`[${new Date().toLocaleString()}] Cron triggered: Processing Clinic Missed Appointments`);
   await processClinicMissedAppointment();
 });
 
 // Schedule to run every day at 10 AM
 cron.schedule('0 10 * * *', async () => {
-  console.log(`[${new Date().toLocaleString()}] Cron triggered: Processing Community Upcoming Appointments`);
+  logging(`[Info]`, `[${new Date().toLocaleString()}] Cron triggered: Processing Community Upcoming Appointments`);
   await processCommunityUpcomingAppointments();
 
-  onsole.log(`[${new Date().toLocaleString()}] Cron triggered: Processing Clinic Before Day Appointments`);
+  logging(`[Info]`, `[${new Date().toLocaleString()}] Cron triggered: Processing Clinic Before Day Appointments`);
   await processClinicBeforeAppointmentDay();
 });
 
 // Schedule to run every day at 6 AM
 cron.schedule('0 6 * * *', async () => {
-    onsole.log(`[${new Date().toLocaleString()}] Cron triggered: Processing Clinic Today Appointments`);
-    await processClinicOnAppointmentDay();
+  logging(`[Info]`, `[${new Date().toLocaleString()}] Cron triggered: Processing Clinic Today Appointments`);
+  await processClinicOnAppointmentDay();
 });
 
 
 // Health check endpoint
 app.get('/health', (req, res) => {
-    res.status(200).send('OK');
+  res.status(200).send('OK');
 });
 
 
 app.listen(process.env.PORT, () => {
-    (process.env.NODE_ENV !== 'prod') ? console.log(`Listening on port ${process.env.PORT}`): ''
-})
+  (process.env.NODE_ENV !== 'prod') ? console.log(`Listening on port ${process.env.PORT}`): ''
+});
